f415e29da5a584a6de100a2b0b8e687c27502a34
Carlos Daniel <162034625+darloscaniel@users.noreply.github.com>
2024-10-08 21:20:16 -0300

    :sparkles: feat: adding images to classes

--- javaFx exercises/classTest/src/main/java/org/example/classtest/app.java
+++ javaFx exercises/classTest/src/main/java/org/example/classtest/app.java
@@ -11,7 +11,7 @@ public class app extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(app.class.getResource("menuPrincipal.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 240, 490);
+        Scene scene = new Scene(fxmlLoader.load(), 318, 565);
         stage.setTitle("Menu Testa Classe");
         stage.setScene(scene);
         stage.show();
